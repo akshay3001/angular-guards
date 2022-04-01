bd49b40fa594bb38e8d83a8fe612c4b6da48a99b
Akshay <akshay.mhatre8989@gmail.com>
2022-04-01 18:44:47 +0530

    cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { NonAuthenticatedComponent } from './non-authenticated/non-authenticated.component';
-import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  { path: 'users', canActivate: [AuthGuard], component: UsersComponent },
   { path: 'non-auth', component: NonAuthenticatedComponent },
 ];
 
